d4f55c53727817f976ff3819cb69501f4f1304e8
Chad Eline <charleseline@gmail.com>
2026-05-22 20:08:02 +0000

    New sources yaml files for staging layer

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -5,13 +5,18 @@ with
     orders as (
         select * from {{ ref("stg_jaffle_shop__orders")}} 
     ),
+    payments as (
+        select * from {{ ref("stg_stripe__payments")}}
+    ),
     customer_orders as (
         select
             customer_id,
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
-            count(order_id) as number_of_orders
+            count(order_id) as number_of_orders,
+            sum(payment_amount) as lifetime_value
         from orders
+        left join payments using (order_id)
         group by 1
     ),
     final as (
@@ -21,7 +26,8 @@ with
             customers.last_name,
             customer_orders.first_order_date,
             customer_orders.most_recent_order_date,
-            coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            lifetime_value
         from customers
         left join customer_orders using (customer_id)
     )
